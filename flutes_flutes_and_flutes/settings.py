48174c1c0e8ad6486302f3b9f8dbb95051a63507
Emily Page <epageone81@gmail.com>
2024-10-14 15:46:22 +0000

    Added checkout success logic

--- flutes_flutes_and_flutes/settings.py
+++ flutes_flutes_and_flutes/settings.py
@@ -169,9 +169,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#Stripe
+# Stripe
 FREE_DELIVERY_THRESHOLD = 500
 STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'gbp'
+STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
