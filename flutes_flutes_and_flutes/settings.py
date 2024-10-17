282ab4b4f63bb43af704f2e5870b7fe550f87e19
Emily Page <epageone81@gmail.com>
2024-10-17 08:40:56 +0000

    Removed secret key and debug

--- flutes_flutes_and_flutes/settings.py
+++ flutes_flutes_and_flutes/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*-of*qmr^fceb483b0)etzf0)70p&zbi0fpz^rx#ip9@e())*7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ep-flutes-flutes-and-flutes-31bc05f84613.herokuapp.com', 'localhost']
 
