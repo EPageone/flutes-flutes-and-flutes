63a95b0538f9871d5e50ae750179e38bb0c8b613
Emily Page <epageone81@gmail.com>
2024-10-24 09:51:08 +0000

    Updated Email confirmation

--- profiles/views.py
+++ profiles/views.py
@@ -6,17 +6,16 @@ from .forms import UserProfileForm
 
 from checkout.models import Order
 
-
 @login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
-
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+
         else:
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
@@ -31,7 +30,6 @@ def profile(request):
     }
 
     return render(request, template, context)
-
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
