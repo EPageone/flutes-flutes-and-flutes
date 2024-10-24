0730bd157f5340fd8d565ba4191407466b1dd396
Emily Page <epageone81@gmail.com>
2024-10-24 12:53:42 +0000

    Debug True

--- flutes_flutes_and_flutes/settings.py
+++ flutes_flutes_and_flutes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['ep-flutes-flutes-and-flutes-31bc05f84613.herokuapp.com', 'localhost' ]
 
